b006744732fa9a5b697384b69afc10fbd3b477c6
hirurana <zcabhra@ucl.ac.uk>
2018-03-16 21:59:17 +0000

    added json script

--- public/js/ajax-form.js
+++ public/js/ajax-form.js
@@ -8,3 +8,29 @@ function ajaxSubmitForm() {
     }
   })
 }
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+$(document).ready(function() {
+  var id = getUrlParameter("id");
+  var authKey = getUrlParameter("key");
+  if (id && authKey) {
+    var url = '/userdata/' + id + '/' + authKey;
+    $.getJSON(url, function(data) {
+      console.log(data);
+    });
+  }
+});
