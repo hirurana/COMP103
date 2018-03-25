58489437df789d76db961b3fb0ffce17e65254b8
Hiru <zcabhra@ucl.ac.uk>
2018-03-25 19:09:47 +0100

    testing callback

--- public/js/multistageForm.js
+++ public/js/multistageForm.js
@@ -84,81 +84,111 @@ $(".submit").click(function(){
 
 //checkbox js
 $(function () {
-  $('.list-group.checked-list-box .list-group-item').each(function () {
+    $('.list-group.checked-list-box .list-group-item').each(function () {
 
-    // Settings
-    var $widget = $(this),
-    $checkbox = $('<input type="checkbox" class="hidden" />'),
-    color = ($widget.data('color') ? $widget.data('color') : "primary"),
-    style = ($widget.data('style') == "button" ? "btn-" : "list-group-item-"),
-    settings = {
-      on: {
-        icon: 'glyphicon glyphicon-check'
-      },
-      off: {
-        icon: 'glyphicon glyphicon-unchecked'
-      }
-    };
+        // Settings
+        var $widget = $(this),
+            $checkbox = $('<input type="checkbox" class="hidden" />'),
+            color = ($widget.data('color') ? $widget.data('color') : "warning"),
+            style = ($widget.data('style') == "button" ? "btn-" : "list-group-item-"),
+            settings = {
+                on: {
+                    icon: 'glyphicon glyphicon-check'
+                },
+                off: {
+                    icon: 'glyphicon glyphicon-unchecked'
+                }
+            };
 
-    $widget.css('cursor', 'pointer')
-    $widget.append($checkbox);
+        $widget.css('cursor', 'pointer')
+        $widget.append($checkbox);
 
-    // Event Handlers
-    $widget.on('click', function () {
-      $checkbox.prop('checked', !$checkbox.is(':checked'));
-      $checkbox.triggerHandler('change');
-      updateDisplay();
+        // Event Handlers
+        $widget.on('click', function () {
+            $checkbox.prop('checked', !$checkbox.is(':checked'));
+            $checkbox.triggerHandler('change');
+            updateDisplay();
+        });
+        $checkbox.on('change', function () {
+            updateDisplay();
+        });
+
+
+        // Actions
+        function updateDisplay() {
+            var isChecked = $checkbox.is(':checked');
+
+            // Set the button's state
+            $widget.data('state', (isChecked) ? "on" : "off");
+
+            // Set the button's icon
+            $widget.find('.state-icon')
+                .removeClass()
+                .addClass('state-icon ' + settings[$widget.data('state')].icon);
+
+            // Update the button's color
+            if (isChecked) {
+                $widget.addClass(style + color + ' active');
+            } else {
+                $widget.removeClass(style + color + ' active');
+            }
+        }
+
+        // Initialization
+        function init() {
+
+            if ($widget.data('checked') == true) {
+                $checkbox.prop('checked', !$checkbox.is(':checked'));
+            }
+
+            updateDisplay();
+
+            // Inject the icon if applicable
+            if ($widget.find('.state-icon').length == 0) {
+                $widget.prepend('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
+            }
+        }
+        init();
     });
-    $checkbox.on('change', function () {
-      updateDisplay();
+
+    $('#get-checked-data').on('click', function(event) {
+        event.preventDefault();
+        var checkedItems = {}, counter = 0;
+        $("#check-list-box li.active").each(function(idx, li) {
+            checkedItems[counter] = $(li).text();
+            counter++;
+        });
+        $('#display-json').html(JSON.stringify(checkedItems, null, '\t'));
     });
-
-
-    // Actions
-    function updateDisplay() {
-      var isChecked = $checkbox.is(':checked');
-
-      // Set the button's state
-      $widget.data('state', (isChecked) ? "on" : "off");
-
-      // Set the button's icon
-      $widget.find('.state-icon')
-      .removeClass()
-      .addClass('state-icon ' + settings[$widget.data('state')].icon);
-
-      // Update the button's color
-      if (isChecked) {
-        $widget.addClass(style + color + ' active');
-      } else {
-        $widget.removeClass(style + color + ' active');
-      }
-    }
-
-    // Initialization
-    function init() {
-
-      if ($widget.data('checked') == true) {
-        $checkbox.prop('checked', !$checkbox.is(':checked'));
-      }
-
-      updateDisplay();
-
-      // Inject the icon if applicable
-      if ($widget.find('.state-icon').length == 0) {
-        $widget.prepend('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
-      }
-    }
-    init();
-  });
-
-  $('#get-checked-data').on('click', function(event) {
-    event.preventDefault();
-    var checkedItems = {}, counter = 0;
-    $("#check-list-box li.active").each(function(idx, li) {
-      checkedItems[counter] = $(li).text();
-      counter++;
-    });
-    $('#display-json').html(JSON.stringify(checkedItems, null, '\t'));
-  });
-
 });
+
+
+
+
+// var getUrlParameter = function getUrlParameter(sParam) {
+//   var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+//   sURLVariables = sPageURL.split('&'),
+//   sParameterName,
+//   i;
+//
+//   for (i = 0; i < sURLVariables.length; i++) {
+//     sParameterName = sURLVariables[i].split('=');
+//
+//     if (sParameterName[0] === sParam) {
+//       return sParameterName[1] === undefined ? true : sParameterName[1];
+//     }
+//   }
+// };
+//
+// $(document).ready(function() {
+//   var id = getUrlParameter("id");
+//   var authKey = getUrlParameter("key");
+//   var url = '/userdata/' + id + '/' + authKey;
+//   $.getJSON(url, function(data) {
+//     console.log(data);
+//     if (data["ok"] == true) {
+//       $('#full_name').html(data["name"]);
+//       $('#department').html(data["department"]);
+//     }
+//   });
+// });
--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,38 @@
 const db = require("../lib/db")
 const path = require('path')
 
-module.exports = function(app) {
-	app.get("/", function(req, res) {
-		res.redirect("/project/6f404e57-4407-4849-bec3-689ef714a206")
-		// res.redirect('/authorise')
-	})
+// TODO: add to user name and upi
 
-	app.get("/project/:projectID", function(req, res) {
-		const projects = db.getProjects()
-		const project = db.getProject(req.params.projectID)
-		if (project == null) {
-			res.redirect("/404")
-		}
-		res.render("project.html", {
-			projects,
-			project,
-			currentProjectID: req.params.projectID
-		})
-	})
+
+module.exports = function(app) {
+	// app.get("/", function(req, res) {
+	// 	res.redirect("/project/6f404e57-4407-4849-bec3-689ef714a206")
+	// 	// res.redirect('/authorise')
+	// })
+
+	// TODO: get projects from upi
+	app.get("/", function(req, res) {
+		db.loadProjects(upi ,function (content) {
+			if (content != null) {
+				res.redirect("/404")
+			}
+		});
+
+
+
+		// const projects = db.getProjects()
+		// const project = db.getProject(req.params.projectID)
+		// if (project == null) {
+		// 	res.redirect("/404")
+		// }
+		// res.render("project.html", {
+		// 	projects,
+		// 	project,
+		// 	currentProjectID: req.params.projectID
+		// })
+	});
 
 	app.post("/project/:projectID/save", function(req, res) {
-		// console.log(req.params, req)
 		db.editProject(req.params.projectID, {
 			projectName: req.body.projectName,
 			projectManager: req.body.projectManager,
@@ -29,12 +40,20 @@ module.exports = function(app) {
 			email: req.body.email,
 			teamMembers: req.body.teamMembers,
 			budget: req.body.budget,
-			approver: req.body.approver
+			approver: req.body.approver,
+			videoURL: req.body.videoURL
 		})
 		res.send("")
 	})
 
+	// TODO: create new project inside MongoDB
 	app.post("/project/new", function(req, res) {
+		const projectID = db.createProject("Untitled 1")
+		res.redirect(`/project/${projectID}`)
+	})
+
+	// TODO: create a delete function in db
+	app.post("/project/delete", function(req, res) {
 		const projectID = db.createProject("Untitled 1")
 		res.redirect(`/project/${projectID}`)
 	})
@@ -43,84 +62,86 @@ module.exports = function(app) {
 		var file = __dirname + '/../public/res/Templates.zip';
 		res.download(file);
 	})
-
 	app.get('/download/guides', function(req, res){
 		var file = __dirname + '/../public/res/Guides.zip';
 		res.download(file);
 	})
-
 	app.get('/download/suppliers', function(req, res){
 		var file = __dirname + '/../public/res/Supplier\ table.pdf';
 		res.download(file);
 	})
 
-
+	app.get('/sendToApprover', function (req, res) {
+		// TODO: call save flow
+		// TODO: get approver email
+		// TODO: call function to send Email
+		// TODO: redirect user to complete page
+	});
 
 	// server.js
 	// where your node app starts
 
 	// init project
-	var randomstring = require("randomstring");
-	// const util = require('util');
-	var moment = require('moment');
-	var getJSON = require('get-json');
-	const nodeRequest = require('request');
-	const util = require("util")
+	// var randomstring = require("randomstring");
+	// // const util = require('util');
+	// var moment = require('moment');
+	// const nodeRequest = require('request');
+	// const util = require("util")
+	//
+	// const client_id = "2824966001992944.0374225578827717"
+	// const client_secret = "3ef5baff0b02b61666106e29eea06b1797403074c63313f470cb9a3a3e5b2c33"
+	//
+	// var states = { };
 
-	const client_id = "2824966001992944.0374225578827717"
-	const client_secret = "3ef5baff0b02b61666106e29eea06b1797403074c63313f470cb9a3a3e5b2c33"
-
-	var states = { };
-
-	//OAuth stuff
-	app.get("/authorise", function(req, res) {
-		var state = randomstring.generate();
-		states[state] = moment();
-		var url = util.format('https://uclapi.com/oauth/authorise?client_id=%s&state=%s', client_id, state);
-		res.redirect(url);
-	});
-
-	app.get("/callback", function(req, res) {
-		var timeNow = moment();
-		if (req.query.state in states) {
-			if (moment(states[req.query.state]).add(300, 'seconds') > timeNow) {
-				if (req.query.result == "denied") {
-					var deniedText = `The login operation for state ${req.query.state} was denied`
-					res.send(deniedText);
-				} else {
-					// Successful login
-					var tokenUrl = util.format('https://uclapi.com/oauth/token?client_id=%s&client_secret=%s&code=%s', client_id, client_secret, req.query.code);
-					console.log("Token URL: " + tokenUrl);
-					var token = "";
-					var name = "";
-					nodeRequest(tokenUrl, { json: true }, (err, res, body) => {
-						if (err) { return console.log(err); }
-						token = body.token;
-						console.log("Got token: " + token);
-						var userDataUrl = util.format('https://uclapi.com/oauth/user/data?client_secret=%s&token=%s', client_secret, token);
-						nodeRequest(userDataUrl, {json: true}, (err, res, body) => {
-							if (err) { return console.log(err); }
-							name = body.full_name;
-							var protectionKey = randomstring.generate();
-							var user = {
-								"name": body.full_name,
-								"department": body.department,
-								"token": token,
-								"auth_key": protectionKey,
-								"upi": body.upi
-							}
-							console.log(body);
-							// TODO: add db shit
-
-							res.redirect(redirectUrl);
-						});
-					});
-				}
-			} else {
-				res.send("Authorisation took more than 5 minutes, so it has failed");
-			}
-		} else {
-			res.send("state does not exist");
-		}
-	});
+	// //OAuth stuff
+	// app.get("/authorise", function(req, res) {
+	// 	var state = randomstring.generate();
+	// 	states[state] = moment();
+	// 	var url = util.format('https://uclapi.com/oauth/authorise?client_id=%s&state=%s', client_id, state);
+	// 	res.redirect(url);
+	// });
+	//
+	// app.get("/callback", function(req, res) {
+	// 	var timeNow = moment();
+	// 	if (req.query.state in states) {
+	// 		if (moment(states[req.query.state]).add(300, 'seconds') > timeNow) {
+	// 			if (req.query.result == "denied") {
+	// 				var deniedText = `The login operation for state ${req.query.state} was denied`
+	// 				res.send(deniedText);
+	// 			} else {
+	// 				// Successful login
+	// 				var tokenUrl = util.format('https://uclapi.com/oauth/token?client_id=%s&client_secret=%s&code=%s', client_id, client_secret, req.query.code);
+	// 				console.log("Token URL: " + tokenUrl);
+	// 				var token = "";
+	// 				var name = "";
+	// 				nodeRequest(tokenUrl, { json: true }, (err, res, body) => {
+	// 					if (err) { return console.log(err); }
+	// 					token = body.token;
+	// 					console.log("Got token: " + token);
+	// 					var userDataUrl = util.format('https://uclapi.com/oauth/user/data?client_secret=%s&token=%s', client_secret, token);
+	// 					nodeRequest(userDataUrl, {json: true}, (err, res, body) => {
+	// 						if (err) { return console.log(err); }
+	// 						name = body.full_name;
+	// 						var protectionKey = randomstring.generate();
+	// 						var user = {
+	// 							"name": body.full_name,
+	// 							"department": body.department,
+	// 							"token": token,
+	// 							"auth_key": protectionKey,
+	// 							"upi": body.upi
+	// 						}
+	// 						console.log(body);
+	// 						// TODO: add db stuff upi can only be accessed here
+	// 						// db.checkUser(body.upi)
+	// 						res.redirect(redirectUrl);
+	// 					});
+	// 				});
+	// 			}
+	// 		} else {
+	// 			res.send("Authorisation took more than 5 minutes, so it has failed");
+	// 		}
+	// 	} else {
+	// 		res.send("state does not exist");
+	// 	}
+	// });
 	}
