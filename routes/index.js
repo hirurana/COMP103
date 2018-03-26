c3abf17a4df638989a39f1ebc8237b22e275a380
Hiru <zcabhra@ucl.ac.uk>
2018-03-26 13:03:26 +0100

    started work on OAuth

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,23 @@ const db = require("../lib/db")
 const path = require('path')
 
 // TODO: add to user name and upi
-var upi = "hrana90";
+// var upi = "hrana90";
 
 module.exports = function(app) {
-	// app.get("/", function(req, res) {
-	// 	res.redirect("/project/6f404e57-4407-4849-bec3-689ef714a206")
-	// 	// res.redirect('/authorise')
-	// })
+	app.get("/", function(req, res) {
+		// res.redirect("/project/6f404e57-4407-4849-bec3-689ef714a206")
+		res.redirect('/authorise');
+	})
 
 	// TODO: get projects from upi
-	app.get("/", function(req, res) {
-		db.loadProjects(upi ,function (content) {
-			if (content != null) {
-				console.log(content);
-				res.redirect("https://www.youtube.com/watch?v=5IWxISz7vYk")
-			}
-		});
+	// app.get("/", function(req, res) {
+	// 	db.loadProjects(upi ,function (projects) {
+	// 		if (projects != null) {
+	// 			console.log(projects);
+	// 			// TODO: get a specific project and store json in a variable
+	// 			res.redirect("https://www.youtube.com/watch?v=5IWxISz7vYk")
+	// 		}
+	// 	});
 
 
 
@@ -95,54 +96,69 @@ module.exports = function(app) {
 	// var states = { };
 
 	// //OAuth stuff
-	// app.get("/authorise", function(req, res) {
-	// 	var state = randomstring.generate();
-	// 	states[state] = moment();
-	// 	var url = util.format('https://uclapi.com/oauth/authorise?client_id=%s&state=%s', client_id, state);
-	// 	res.redirect(url);
-	// });
-	//
-	// app.get("/callback", function(req, res) {
-	// 	var timeNow = moment();
-	// 	if (req.query.state in states) {
-	// 		if (moment(states[req.query.state]).add(300, 'seconds') > timeNow) {
-	// 			if (req.query.result == "denied") {
-	// 				var deniedText = `The login operation for state ${req.query.state} was denied`
-	// 				res.send(deniedText);
-	// 			} else {
-	// 				// Successful login
-	// 				var tokenUrl = util.format('https://uclapi.com/oauth/token?client_id=%s&client_secret=%s&code=%s', client_id, client_secret, req.query.code);
-	// 				console.log("Token URL: " + tokenUrl);
-	// 				var token = "";
-	// 				var name = "";
-	// 				nodeRequest(tokenUrl, { json: true }, (err, res, body) => {
-	// 					if (err) { return console.log(err); }
-	// 					token = body.token;
-	// 					console.log("Got token: " + token);
-	// 					var userDataUrl = util.format('https://uclapi.com/oauth/user/data?client_secret=%s&token=%s', client_secret, token);
-	// 					nodeRequest(userDataUrl, {json: true}, (err, res, body) => {
-	// 						if (err) { return console.log(err); }
-	// 						name = body.full_name;
-	// 						var protectionKey = randomstring.generate();
-	// 						var user = {
-	// 							"name": body.full_name,
-	// 							"department": body.department,
-	// 							"token": token,
-	// 							"auth_key": protectionKey,
-	// 							"upi": body.upi
-	// 						}
-	// 						console.log(body);
-	// 						// TODO: add db stuff upi can only be accessed here
-	// 						// db.checkUser(body.upi)
-	// 						res.redirect(redirectUrl);
-	// 					});
-	// 				});
-	// 			}
-	// 		} else {
-	// 			res.send("Authorisation took more than 5 minutes, so it has failed");
-	// 		}
-	// 	} else {
-	// 		res.send("state does not exist");
-	// 	}
-	// });
-	}
+	app.get("/authorise", function(req, res) {
+		var state = randomstring.generate();
+		states[state] = moment();
+		var url = util.format('https://uclapi.com/oauth/authorise?client_id=%s&state=%s', client_id, state);
+		res.redirect(url);
+	});
+
+	app.get("/callback", function(req, res) {
+		var timeNow = moment();
+		if (req.query.state in states) {
+			if (moment(states[req.query.state]).add(300, 'seconds') > timeNow) {
+				if (req.query.result == "denied") {
+					var deniedText = `The login operation for state ${req.query.state} was denied`
+					res.send(deniedText);
+				} else {
+					// Successful login
+					var tokenUrl = util.format('https://uclapi.com/oauth/token?client_id=%s&client_secret=%s&code=%s', client_id, client_secret, req.query.code);
+					console.log("Token URL: " + tokenUrl);
+					var token = "";
+					var name = "";
+					nodeRequest(tokenUrl, { json: true }, (err, res, body) => {
+						if (err) { return console.log(err); }
+						token = body.token;
+						console.log("Got token: " + token);
+						var userDataUrl = util.format('https://uclapi.com/oauth/user/data?client_secret=%s&token=%s', client_secret, token);
+						nodeRequest(userDataUrl, {json: true}, (err, res, body) => {
+							if (err) { return console.log(err); }
+							name = body.full_name;
+							var protectionKey = randomstring.generate();
+							var user = {
+								"name": body.full_name,
+								"department": body.department,
+								"token": token,
+								"auth_key": protectionKey,
+								"upi": body.upi
+							}
+							console.log(body);
+							// TODO: add db stuff upi can only be accessed here
+							// db.checkUser(body.upi)
+							// const projects = db.getProjects()
+							// const project = db.getProject(req.params.projectID)
+							// if (project == null) {
+							// 	res.redirect("/404")
+							// }
+							// res.render("project.html", {
+							// 	projects,
+							// 	project,
+							// 	currentProjectID: req.params.projectID
+							// })
+							// db.loadProjects(body.upi, function (projects) {
+							// 	res.render("project.html", {
+							// 		projects,
+							//
+							// 	});
+							// });
+							res.redirect("https://www.youtube.com/watch?v=rdeQT7KkqM8");
+						});
+					});
+				}
+			} else {
+				res.send("Authorisation took more than 5 minutes, so it has failed");
+			}
+		} else {
+			res.send("state does not exist");
+		}
+	});
