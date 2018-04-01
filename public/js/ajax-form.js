f01c543a0e8ce904e005f992e643dc60bc2bbbf2
Médi-Rémi Hashim <mediremi@gmail.com>
2018-04-01 16:30:21 +0100

    Done :)

--- public/js/ajax-form.js
+++ public/js/ajax-form.js
@@ -9,19 +9,23 @@ var checkedItems = {
   necessarySteps: false,
   accurateInfo: false
 }
+
+function updateCheckedItems() {
+  function fn(idx, li) {
+    var $li = $(li)
+    if ($li.hasClass("active")) checkedItems[$li.attr("name")] = true;
+    else checkedItems[$li.attr("name")] = false
+  }
+  $("#check-list-box li").each(fn)
+  $("#check-list-box2 li").each(fn)
+}
+
 $('#check-list-box').on('click', function(event) {
-    $("#check-list-box li").each(function(idx, li) {
-        var $li = $(li)
-        if ($li.hasClass("active")) checkedItems[$li.attr("name")] = true;
-        else checkedItems[$li.attr("name")] = false
-    })
+  updateCheckedItems()
 });
+
 $('#check-list-box2').on('click', function(event) {
-    $("#check-list-box2 li").each(function(idx, li) {
-        var $li = $(li)
-        if ($li.hasClass("active")) checkedItems[$li.attr("name")] = true;
-        else checkedItems[$li.attr("name")] = false
-    })
+  updateCheckedItems()
 });
 
 function ajaxSubmitForm() {
@@ -43,16 +47,16 @@ function ajaxSubmitForm() {
 }
 
 var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
     }
+  }
 };
